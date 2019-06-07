99c13b8e1239da186714ede05e13181325f83c8a
Eddy <eddydevshao@gmail.com>
2019-06-07 13:07:24 +1000

    fixed program not running when no current game
    starting the program would result in key errors because no games are able to be found

--- main.py
+++ main.py
@@ -17,24 +17,51 @@ api = twitter.authentication(
     consumer_key, consumer_secret, access_token, access_token_secret)
 
 # Pretty print the response into a file. Mainly used just for me to figure out the JSON.
-#twitter.openFile(file_path, searchMoves)
+# twitter.openFile(file_path, searchMoves)
 
 
-def search_for_move(query):  # Search for tweets containing the hashtag #bwbchess
+def get_move(json_tweet):  # Search for tweets containing the hashtag #bwbchess
+    try:
+        str_tweet = json_tweet["statuses"][0]["text"]
+        split_tweet = str_tweet.split(" ")
+        move = str(split_tweet[-1])
+        return move
+    except IndexError:
+        print("Move not found!")
+        return False
+
+
+def tweet_query(query):
     latest_tweet = twitter.search(api, query)
     json_tweet = json.loads(json.dumps(latest_tweet))
-    str_tweet = json_tweet["statuses"][0]["text"]
-    split_tweet = str_tweet.split(" ")
-    move = str(split_tweet[-1])
-    return json_tweet, move
+    return json_tweet
 
 
-game_id = lichess.get_game_id(lichess_token)
-json_tweet, move = search_for_move("#bwbchess")
-# Get the first mention in the tweet
-user_mentions = json_tweet["statuses"][0]["entities"]["user_mentions"][0]["screen_name"]
+def user_mentions(tweet_data):
+    try:  # Get the first mention in the tweet
+        print(f"Move is: {move}")
+        user_mentions = tweet_data["statuses"][0]["entities"]["user_mentions"][0]["screen_name"]
+        print(f"User mention is: {user_mentions}")
+        return user_mentions
+    except IndexError:
+        print("No user mentions")
+        return False
 
-if user_mentions == "bwbchess":
-    lichess.make_move(game_id, move, lichess_token)
+
+def make_move():
+    if user_mention == "bwbchess":
+        print("Correct user mention")
+        lichess.make_move(game_id, move, lichess_token)
+    else:
+        print(user_mention)
+        print("Something bad happened and a move was not made!")
+
+
+tweet_data = tweet_query("#bwbchess")
+if get_move(tweet_data) != False:
+    move = get_move(tweet_data)
+    user_mention = user_mentions(tweet_data)
+    game_id = lichess.get_game_id(lichess_token)
+    make_move()
 else:
-    print("Something bad happened!")
+    print("Game, move and user mention not found!")
